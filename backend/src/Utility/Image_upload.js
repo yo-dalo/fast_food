88d0d0b71d07689c2e5db1dd3e68a59646a549ca
Adarshkumarjhook <amitkumarjhookiya@gmail.com>
2024-09-15 19:21:23 +0530

    v2

--- backend/src/Utility/Image_upload.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
--- backend/src/routers/mailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mailController = require('../Utility/Send_email');
-
-// POST route to send an email
-router.post('/send-email', mailController.sendEmail);
-
-module.exports = router;
